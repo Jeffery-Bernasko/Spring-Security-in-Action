5bc7f7e595685e366c2546a7d80607752b61153b
Jeffery Bernasko <jefferypeter20@gmail.com>
2025-06-04 16:31:35 +0000

    managing users

--- src/main/java/org/example/learningspringsecurity/config/ProjectConfig.java
+++ src/main/java/org/example/learningspringsecurity/config/ProjectConfig.java
@@ -2,11 +2,16 @@ package org.example.learningspringsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class ProjectConfig {
@@ -25,7 +30,16 @@ public class ProjectConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(auth -> auth
+                .anyRequest()
+                .permitAll()).httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 }
